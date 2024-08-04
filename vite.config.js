2852bbe977a3f6e6d50908046114169dc294c8a6
Steven Lynn <i@stv.lol>
2024-08-05 00:03:47 +0800

    Revert "	modified:   index.html"
    This reverts commit e97e25984cd4ca055433772d7e4c5c0465dea715.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+  server: {
+    historyApiFallback: true
   }
 })
