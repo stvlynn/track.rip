e97e25984cd4ca055433772d7e4c5c0465dea715
Steven Lynn <i@stv.lol>
2024-08-04 23:53:32 +0800

    modified:   index.html 	modified:   src/main.js 	modified:   vercel.json 	modified:   vite.config.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style.css'
+
+console.log('Vue version:', Vue.version)
+console.log('Current route:', router.currentRoute.value)
 
 const app = createApp(App)
+
+app.config.errorHandler = (err, vm, info) => {
+  console.error('Global error:', err)
+  console.error('Error info:', info)
+}
+
 app.use(router)
-app.mount('#app')
+
+router.isReady().then(() => {
+  app.mount('#app')
+}).catch(error => {
+  console.error('Router error:', error)
+})
