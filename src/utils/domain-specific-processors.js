ec9d7a3277a1a65abd78b82002b8c848eac48092
Steven Lynn <i@stv.lol>
2024-08-04 11:08:45 +0800

    modified:   src/utils/domain-specific-processors.js

--- src/utils/domain-specific-processors.js
+++ src/utils/domain-specific-processors.js
@@ -28,41 +28,35 @@ const processUrlBasedOnDomain = async (url) => {
     finalUrl = await processDefaultUrl(url)
   }
 
-  return finalUrl
+  return `${window.location.origin}/result?url=${encodeURIComponent(finalUrl)}`
 }
 
 async function processXiaoHongShu(url) {
-  // 处理小红书及其短链的逻辑
   let extractedUrl = await extractUrlFromText(url)
   return extractedUrl
 }
 
 async function processWeiXin(url) {
-  // 处理微信公众号的逻辑
   let extractedUrl = await extractUrlFromText(url)
   return extractedUrl
 }
 
 async function processNetEaseMusic(url) {
-  // 处理网易云音乐及其短链的逻辑
   let extractedUrl = await extractUrlFromText(url)
   return extractedUrl
 }
 
 async function processBiliBili(url) {
-  // 处理 B 站及其短链的逻辑
   let extractedUrl = await extractUrlFromText(url)
   return extractedUrl
 }
 
 async function processZhihu(url) {
-  // 处理知乎的逻辑
   let extractedUrl = await extractUrlFromText(url)
   return extractedUrl
 }
 
 async function processDefaultUrl(url) {
-  // 处理其他域名的默认逻辑（清空第一个?后的查询参数）
   let extractedUrl = new URL(url)
   extractedUrl.search = ''
   return extractedUrl.toString()
