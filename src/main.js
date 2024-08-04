c8f09277a489127defcd37b0cf332779e5dfef2d
Steven Lynn <i@stv.lol>
2024-08-05 00:30:44 +0800

    Reapply "	modified:   index.html"
    This reverts commit 2852bbe977a3f6e6d50908046114169dc294c8a6.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './style.css'
+
+console.log('Vue version:', Vue.version)
+console.log('Current route:', router.currentRoute.value)
 
 const app = createApp(App)
 app.use(router)
-app.mount('#app')
+
+router.isReady().then(() => {
+  app.mount('#app')
+}).catch(error => {
+  console.error('Router error:', error)
+})
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    historyApiFallback: true
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
