f66853a0a3b23ff825191423ad21dcd051a7f666
Steven Lynn <i@stv.lol>
2024-08-05 00:36:43 +0800

    Revert "	modified:   index.html"
    This reverts commit e97e25984cd4ca055433772d7e4c5c0465dea715.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
-
-console.log('Vue version:', Vue.version)
-console.log('Current route:', router.currentRoute.value)
 
 const app = createApp(App)
 app.use(router)
-
-router.isReady().then(() => {
-  app.mount('#app')
-}).catch(error => {
-  console.error('Router error:', error)
-})
+app.mount('#app')
