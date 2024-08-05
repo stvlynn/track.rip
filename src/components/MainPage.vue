f0a657c60bc01fffe2031654e942c6276335f417
Steven Lynn <i@stv.lol>
2024-08-06 00:37:39 +0800

    modified:   src/components/MainPage.vue

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,29 +1,23 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen relative">
-    <div class="absolute inset-0 bg-cover bg-center blur-[40%]" style="background-image: url('https://www.loliapi.com/acg/')"></div>
-    <div class="bg-white/90 shadow-md hover:shadow-2xl transition-shadow rounded-[50px] p-8 w-full max-w-md relative z-10">
+    <div class="absolute inset-0 bg-cover bg-center blur-[40%]"
+      style="background-image: url('https://www.loliapi.com/acg/')"></div>
+    <div
+      class="bg-white/90 shadow-md hover:shadow-2xl transition-shadow rounded-[50px] p-8 w-full max-w-md relative z-10">
       <h1 class="text-4xl font-bold mb-8 text-center">去除追踪参数</h1>
       <div class="relative">
-        <input
-          v-model="inputUrl"
-          type="text"
-          class="border border-gray-300 rounded-[50px] px-4 py-3 w-full pr-12 text-center"
-          placeholder="请输入链接"
-        />
-        <button
-          @click="pasteUrl"
-          class="absolute top-1/2 -translate-y-1/2 right-4 text-gray-500 hover:text-gray-700"
-        >
+        <input v-model="inputUrl" type="text"
+          class="border border-gray-300 rounded-[50px] px-4 py-3 w-full pr-12 text-center" placeholder="请输入链接" />
+        <button @click="pasteUrl" class="absolute top-1/2 -translate-y-1/2 right-4 text-gray-500 hover:text-gray-700">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
           </svg>
         </button>
       </div>
       <div class="mt-4"></div>
-      <button
-        @click="processUrl"
-        class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-bold py-3 px-6 rounded-[50px] w-full"
-      >
+      <button @click="processUrl"
+        class="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white font-bold py-3 px-6 rounded-[50px] w-full">
         处理
       </button>
       <div class="mt-8 space-y-2 text-center">
@@ -51,6 +45,9 @@
           其他域名采用默认处理逻辑（清空第一个?后的查询参数）
         </div>
       </div>
+      <div class="flex justify-center mt-8">
+        <iframe src="https://ghbtns.com/github-btn.html?user=stvlynn&repo=track.rip&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
+      </div>
     </div>
   </div>
 </template>
@@ -69,10 +66,10 @@ export default {
     // Check if there's a URL in the route params
     if (this.$route.params.url) {
       this.inputUrl = this.$route.params.url
-      if(this.inputUrl.includes(':/') && !this.inputUrl.includes('://')){
+      if (this.inputUrl.includes(':/') && !this.inputUrl.includes('://')) {
         this.inputUrl = this.inputUrl.replace(':/', '://')
       }
-      if(!this.inputUrl.startsWith('http://') && !this.inputUrl.startsWith('https://')){
+      if (!this.inputUrl.startsWith('http://') && !this.inputUrl.startsWith('https://')) {
         this.inputUrl = 'http://' + this.inputUrl
       }
       this.processUrlFromParams(this.inputUrl)
